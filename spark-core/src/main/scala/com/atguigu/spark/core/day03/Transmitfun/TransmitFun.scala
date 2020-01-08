1c7fd1df780eeac1460333c0490e1950cd622d54
John Doe <30752740@qq.com>
2020-01-08 10:51:25 +0800

    update

--- spark-core/src/main/scala/com/atguigu/spark/core/day03/Transmitfun/TransmitFun.scala
+++ spark-core/src/main/scala/com/atguigu/spark/core/day03/Transmitfun/TransmitFun.scala
@@ -1,41 +1,49 @@
 package com.atguigu.spark.core.day03.Transmitfun
 
 
-import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
 
 object TransmitFun {
     def main(args: Array[String]): Unit = {
         val conf: SparkConf = new SparkConf()
             .setAppName("SerDemo")
             .setMaster("local[*]")
-            
+            // set("spark.serializer", classOf[KryoSerializer].getName)  org.apache.spark.serializer.KryoSerializer
+            .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")  // 多余
+            .registerKryoClasses(Array(classOf[Searcher]))
+        
         val sc = new SparkContext(conf)
         val rdd: RDD[String] = sc.parallelize(Array("hello world", "hello atguigu", "atguigu", "hahah"), 2)
-        val searcher = new Searcher("hello")  // 对象是在driver 创建
-        val result: RDD[String] = searcher.getMatchedRDD3(rdd)
+        val searcher = new Searcher("hello") // 对象是在driver 创建
+        val result: RDD[String] = searcher.getMatchedRDD1(rdd)
         result.collect.foreach(println)
         // kryo 通用的序列化机制
+        new HashPartitioner(3)
     }
 }
+
 //需求: 在 RDD 中查找出来包含 query 子字符串的元素
 
 // query 为需要查找的子字符串
-class Searcher(val query: String){
+class Searcher(val query: String) extends Serializable {
     
     // 判断 s 中是否包括子字符串 query
-    def isMatch(s : String): Boolean ={
+    def isMatch(s: String): Boolean = {
         s.contains(query)
         
     }
+    
     // 过滤出包含 query字符串的字符串组成的新的 RDD  需要类实现Serializable
-    def getMatchedRDD1(rdd: RDD[String]): RDD[String] ={
-        rdd.filter(isMatch)  //
+    def getMatchedRDD1(rdd: RDD[String]): RDD[String] = {
+        rdd.filter(isMatch) //
     }
+    
     // 需要类实现Serializable
     def getMatchedRDD2(rdd: RDD[String]): RDD[String] = {
         rdd.filter(s => s.contains(query))
     }
+    
     // 不需要实现Serializable
     def getMatchedRDD3(rdd: RDD[String]): RDD[String] = {
         val q: String = this.query
